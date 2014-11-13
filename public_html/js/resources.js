b7ee1d4201f7d593d7f906feea2741b74993e6f6
katalina arce <katalinaarce34@gmail.com>
2014-11-13 09:22:08 -0800

    02

--- public_html/js/resources.js
+++ public_html/js/resources.js
@@ -1,28 +1,28 @@
 game.resources = [
+    /* Graphics. 
+     * @example
+     * {name: "example", type:"image", src: "data/img/example.png"},
+     */
+    {name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},
+    {name: "mario", type: "image", src: "data/img/player1.png"},
+    /* Atlases 
+     * @example
+     * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+     */
 
-	/* Graphics. 
-	 * @example
-	 * {name: "example", type:"image", src: "data/img/example.png"},
-	 */
+    /* Maps. 
+     * @example
+     * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+     * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+     */
+    {name: "Katlevel01", type: "tmx", src: "data/map/Katlevel01.tmx"}
+    /* Background music. 
+     * @example
+     * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+     */
 
-	/* Atlases 
-	 * @example
-	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
-	 */
-		
-	/* Maps. 
-	 * @example
-	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
-	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
- 	 */
-
-	/* Background music. 
-	 * @example
-	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
-	 */	
-
-	/* Sound effects. 
-	 * @example
-	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
-	 */
+    /* Sound effects. 
+     * @example
+     * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+     */
 ];
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -1,22 +1,25 @@
 game.PlayScreen = me.ScreenObject.extend({
-	/**
-	 *  action to perform on state change
-	 */
-	onResetEvent: function() {
-		// reset the score
-		game.data.score = 0;
+    /**
+     *  action to perform on state change
+     */
+    onResetEvent: function() {
+        // reset the score
+        game.data.score = 0;
 
-		// add our HUD to the game world
-		this.HUD = new game.HUD.Container();
-		me.game.world.addChild(this.HUD);
-	},
+        me.levelDirector.loadLevel("Katlevel01");
 
-
-	/**
-	 *  action to perform when leaving this screen (state change)
-	 */
-	onDestroyEvent: function() {
-		// remove the HUD from the game world
-		me.game.world.removeChild(this.HUD);
-	}
+var player = me.pool.pull("mario", 0, 420, {});
+me.game.world.addChild(player, 3);
+        
+        // add our HUD to the game world
+        this.HUD = new game.HUD.Container();
+        me.game.world.addChild(this.HUD);
+    },
+    /**
+     *  action to perform when leaving this screen (state change)
+     */
+    onDestroyEvent: function() {
+        // remove the HUD from the game world
+        me.game.world.removeChild(this.HUD);
+    }
 });
